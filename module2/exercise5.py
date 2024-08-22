4df411bd35c6605966e213479cc7f5e93e5a41fb
wolvs5 <krish.shrestha@metropolia.fi>
2024-08-22 10:31:29 +0300

    exercises

---
+++ module2/exercise5.py
@@ -0,0 +1,15 @@
+#Write a program that asks the user to enter a mass in medieval units: talents (leiviskä),
+# pounds (naula), and lots (luoti).
+# The program converts the input to full kilograms and grams and
+
+
+#One talent is 20 pounds.
+#One pound is 32 lots.
+#One lot is 13,3 grams.
+talent=float(input("Enter your Talent: "))
+pounds=float(input("Enter your Pounds: "))
+lot=float(input("Enter your Lot: "))
+kg= (talent*20*32*13.3 + pounds*32*13.3 + lot*13.3)
+kg1=kg//1000
+gram=kg%1000
+print(f"The weight in modern units:\n{kg1} kilograms and {gram} grams")
