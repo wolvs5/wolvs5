f660f5d7adbce0ea9aadc7c3765b0ed7b71c5234
wolvs5 <krish.shrestha@metropolia.fi>
2024-11-30 18:51:40 +0200

    Update esercise13-1.py

--- module 13/esercise13-1.py
+++ module 13/esercise13-1.py
@@ -2,8 +2,6 @@ from flask import Flask, jsonify
 import math
 
 app = Flask(__name__)
-
-# Function to check if a number is prime
 def is_prime(n):
     if n <= 1:
         return False
@@ -12,21 +10,19 @@ def is_prime(n):
             return False
     return True
 
-# Endpoint to check if a number is prime
+
 @app.route('/prime_number/<int:number>', methods=['GET'])
 def check_prime(number):
     prime_status = is_prime(number)
     return jsonify({"Number": number, "isPrime": prime_status})
 
-# Sample airport data (in a real application, this would be in a database)
 airport_data = {
     "EFHK": {"Name": "Helsinki-Vantaa Airport", "Location": "Helsinki"},
     "EGLL": {"Name": "London Heathrow Airport", "Location": "London"},
     "KATL": {"Name": "Hartsfield-Jackson Atlanta International Airport", "Location": "Atlanta"},
-    # Add more airports as needed
+  
 }
 
-# Endpoint to get airport information by ICAO code
 @app.route('/airport/<icao_code>', methods=['GET'])
 def get_airport(icao_code):
     airport = airport_data.get(icao_code)
